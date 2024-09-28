81061fb712dc186c675051953bf781e53b3b0dc2
pavin <pavin227@gmail.com>
2024-09-28 22:30:02 +0530

    Added new origin

--- config/allowerdOrigins.js
+++ config/allowerdOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3500', 
-    'https://www.yoursite.com'
+    'https://www.yoursite.com',
+    'http://localhost:3000'
 ]
 
 module.exports = allowedOrigins
