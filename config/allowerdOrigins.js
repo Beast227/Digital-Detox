a7d383c9d61bebd980c1272ebd91899676e7b3cf
pavin <pavin227@gmail.com>
2024-10-02 17:55:12 +0530

    Added new origin

--- config/allowerdOrigins.js
+++ config/allowerdOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3500', 
     'http://localhost:3000',
-    'https://yash29739.github.io/'
+    'https://yash29739.github.io'
 ]
 
 module.exports = allowedOrigins
