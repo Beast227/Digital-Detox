a1f62864c37b96ed4b228f379301cb0eaefd0f25
pavin <pavin227@gmail.com>
2024-11-01 19:40:45 +0530

    Correction

--- controlllers/authController.js
+++ controlllers/authController.js
@@ -68,7 +68,7 @@ const handleLogin = async (req, res) => {
     // Set the new refresh token in the client-side cookie
     res.cookie('jwt', newRefreshToken, {
         httpOnly: true,
-        sameSite: 'Lax', // Set 'Secure' and 'SameSite' options properly in production 
+        sameSite: 'None', // Set 'Secure' and 'SameSite' options properly in production 
         secure: true,// For production, ensure HTTPS
         maxAge: 24 * 60 * 60 * 1000 // Cookie expires in 1 day
     });
